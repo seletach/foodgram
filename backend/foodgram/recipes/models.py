10a668de5f5840dc1ed2a9f5b3cc021495822ed5
Ilya <ilkanov106@gmail.com>
2025-01-02 18:20:41 +0800

    перед миграциями

---
+++ backend/foodgram/recipes/models.py
@@ -0,0 +1,170 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
+from django.db import models
+
+User = get_user_model()
+
+
+class Tag(models.Model):
+    """Модель тега"""
+
+    name = models.CharField(
+        verbose_name='Название тега',
+        max_length=32,
+        help_text='Не более 32 символов',
+        unique=True
+    )
+    slug = models.SlugField(
+        verbose_name='Название slug',
+        max_length=32,
+        help_text='slug должен быть уникальным',
+        unique=True
+    )
+
+    class Meta:
+        verbose_name='Тег'
+        verbose_name_plural='Теги'
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Модель ингредиента"""
+
+    name = models.CharField(
+        verbose_name='Название ингредиента',
+        max_length=50,
+        help_text='Не более 50 символов'
+    )
+    measurement_unit = models.CharField(
+        verbose_name='Единица измерения',
+        max_length=50,
+        help_text='Не более 50 символов'
+    )
+
+    class Meta:
+        verbose_name='Ингредиент'
+        verbose_name_plural='Ингредиенты'
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    """Модель рецепта"""
+
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор рецепта',
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+    name = models.CharField(
+        verbose_name='Название рецепта',
+        max_length=50,
+        help_text='Не более 50 символов'
+    )
+    text = models.TextField(
+        verbose_name='Описание рецепта'
+    )
+    created = models.DateTimeField(
+        verbose_name='Дата публикации рецепта',
+        auto_now_add=True
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Время приготовления в минутах',
+        validators=[MinValueValidator(1)]
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='IngredientsInRecipe',
+        verbose_name='Ингредиенты',
+        help_text='Минимум 2 ингредиента | ',
+        validators=[MinValueValidator(2)],
+        related_name='recipes'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        help_text='Выберите не менее одного тега | ',
+        validators=[MinValueValidator(1)],
+        related_name='recipes'
+    )
+    image = models.ImageField(
+        verbose_name='Картинка',
+        upload_to='recipes/',
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = ['-created', 'author']
+
+    def __str__(self):
+        return self.name
+
+
+class IngredientsInRecipe(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        verbose_name='Рецепт',
+        on_delete=models.CASCADE
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        verbose_name='Ингредиент',
+        on_delete=models.CASCADE
+    )
+    amount = models.PositiveSmallIntegerField(
+        verbose_name='Количество',
+        validators=[MinValueValidator(1)]
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент в рецепте'
+        verbose_name_plural = 'Ингредиенты в рецептах'
+    
+    def __str__(self):
+        return f'{self.ingredient} {self.recipe}'
+
+
+class ShoppingCart(models.Model):
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Владелец корзины'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт'
+    )
+
+    class Meta:
+        verbose_name = 'Список покупок'
+        verbose_name_plural = 'Списки покупок'
+    
+    def __str__(self):
+        return f'{self.owner} {self.recipe}'
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт'
+    )
+
+    class Meta:
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранное'
+
+    def __str__(self):
+        return f'{self.user} {self.recipe}'
---
+++ backend/foodgram/users/models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    """Модифицированная модель пользователя"""
+
+    email = models.EmailField(max_length=254, unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
+
+class Subscriptions(models.Model):
+    """Модель подписок пользователя на других пользователей"""
+
+    author = models.ForeignKey(
+        CustomUser,
+        verbose_name='Автор',
+        on_delete=models.CASCADE
+    )
+    subscriber = models.ForeignKey(
+        CustomUser,
+        verbose_name='Подписчик',
+        related_name='subscriber',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self):
+        return f'{self.subscriber.username} подписан на {self.author.username}'
