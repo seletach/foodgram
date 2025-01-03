a23b28ed7c3cdcb8b26eb3186c879cd8c00e3a54
Ilya <ilkanov106@gmail.com>
2025-01-03 16:36:13 +0800

    перед заполнением данных в БД

---
+++ backend/foodgram/import_data.py
@@ -0,0 +1,23 @@
+import csv
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram.settings')
+django.setup()
+
+from recipes.models import Ingredient
+
+def import_ingredients_from_csv(file_path):
+    with open(file_path, mode='r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            ingredient = Ingredient(
+                name=row[0],
+                measurement_unit=row[1]
+            )
+            ingredient.save()
+            print(f'Added ingredient: {ingredient}')
+
+if __name__ == '__main__':
+    file_path = '/home/ilya/develop/foodgram3/foodgram/data/ingredients.csv'
+    import_ingredients_from_csv(file_path)
