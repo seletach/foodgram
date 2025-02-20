a3f6bff96e0289aebf5dd39ee3ad9e76b7542dfb
Ilya <ilkanov106@gmail.com>
2025-02-21 05:31:06 +0800

    commit

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -161,3 +161,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.CustomUser'
 
 SITE_URL = os.getenv('SITE_URL', 'localhost')
+
+CSRF_TRUSTED_ORIGINS = ['https://foodgram-seletach.zapto.org/']
