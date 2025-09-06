923c25ed59be3164205d2b3db600c500c4917a32
Ilya <ilkanov106@gmail.com>
2025-09-06 16:14:02 +0800

    istartswith

---
+++ backend/import_data.py
@@ -0,0 +1,22 @@
+import csv
+import django
+import os
+
+from recipes.models import Ingredient
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram.settings')
+django.setup()
+
+
+def import_ingredients_from_csv(file_path):
+    with open(file_path, mode='r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            ingredient = Ingredient(name=row[0], measurement_unit=row[1])
+            ingredient.save()
+            print(f'Added ingredient: {ingredient}')
+
+
+if __name__ == '__main__':
+    file_path = '/app/data/ingredients.csv'
+    import_ingredients_from_csv(file_path)
